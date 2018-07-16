f53ce32e35befe8eb88cfa5b6ec94d7320ee5484
duanyong <947638768@qq.com>
2018-07-16 17:07:56 +0800

    add faster rcnn

--- lib/nets/vgg16.py
+++ lib/nets/vgg16.py
@@ -29,7 +29,7 @@ class Vgg16(Network):
             net = slim.max_pool2d(net, [2, 2], padding='SAME', scope='pool2')
             net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], trainable=is_training, scope='conv3')
             net = slim.max_pool2d(net, [2, 2], padding='SAME', scope='pool3')
-            net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], trainable=is_training, scope='conv4')
+            net = slim.repeat(net, 3    , slim.conv2d, 512, [3, 3], trainable=is_training, scope='conv4')
             net = slim.max_pool2d(net, [2, 2], padding='SAME', scope='pool4')
             net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], trainable=is_training, scope='conv5')
             # net = slim.max_pool2d(net, [2, 2], padding='SAME', scope='pool5')
@@ -51,6 +51,25 @@ class Vgg16(Network):
                 fc7 = slim.dropout(fc7, keep_prob=0.5, is_training=True, scope='dropout7')
         return fc7
 
+    def get_variables_to_restore(self, variables, var_keep_dic):
+        variables_to_restore = []
+
+        for v in variables:
+            # exclude the conv weights that are fc weights in vgg16
+            if v.name == (self._scope + '/fc6/weights:0') or \
+                    v.name == (self._scope + '/fc7/weights:0'):
+                self._variables_to_transform[v.name] = v
+                continue
+            # exclude the first conv layer to swap RGB to BGR
+            if v.name == (self._scope + '/conv1/conv1_1/weights:0'):
+                self._variables_to_transform[v.name] = v
+                continue
+            if v.name.split(':')[0] in var_keep_dic:
+                print('Variables restored: %s' % v.name)
+                variables_to_restore.append(v)
+
+        return variables_to_restore
+
     def transform_variables(self, sess, pretrained_model):
         """
         TODO 分别试试转换后的效果和不转换的效果
@@ -68,11 +87,11 @@ class Vgg16(Network):
                                                      self._scope + "/conv1/conv1_1/weights": conv1_rgb})
                 restorer_variables.restore(sess, pretrained_model)
 
-                sess.run(tf.assign(self._variables_to_fix[self._scope + '/fc6/weights:0'],
-                                   tf.reshape(fc6_conv, self._variables_to_fix[
+                sess.run(tf.assign(self._variables_to_transform[self._scope + '/fc6/weights:0'],
+                                   tf.reshape(fc6_conv, self._variables_to_transform[
                                        self._scope + '/fc6/weights:0'].get_shape())))
-                sess.run(tf.assign(self._variables_to_fix[self._scope + '/fc7/weights:0'],
-                                   tf.reshape(fc7_conv, self._variables_to_fix[
+                sess.run(tf.assign(self._variables_to_transform[self._scope + '/fc7/weights:0'],
+                                   tf.reshape(fc7_conv, self._variables_to_transform[
                                        self._scope + '/fc7/weights:0'].get_shape())))
-                sess.run(tf.assign(self._variables_to_fix[self._scope + '/conv1/conv1_1/weights:0'],
+                sess.run(tf.assign(self._variables_to_transform[self._scope + '/conv1/conv1_1/weights:0'],
                                    tf.reverse(conv1_rgb, [2])))
