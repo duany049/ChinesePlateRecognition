4b0490d1654bfef057428795dc925b748b63bb4a
duany049 <947638768@qq.com>
2018-07-19 00:01:10 +0800

    add transform format

--- data/transfrom_format/transform_format.py
+++ data/transfrom_format/transform_format.py
@@ -20,19 +20,20 @@ import matplotlib.pyplot as plt
 #     test_image.show()
 
 
-def generate_coor():
+def generate_format_img():
     """
     左上坐标：x,y分别取四个坐标中x,y最小值
     右下坐标：x,y分别取四个坐标中x,y最大值
-    :return: 左上右下的坐标
+    :return: 车牌内容以及左上右下的坐标
     """
     cur_path = os.path.dirname(__file__)
     imgs_src_dir = os.path.join(cur_path, "imgs_src")
     images_dir = os.path.join(cur_path, 'transformed_imgs', 'JPEGImages')
     imgs_name_list = os.listdir(imgs_src_dir)
+    print(imgs_name_list)
     for image_name in imgs_name_list:
         if not '_' in image_name:
-            print('ignore the file, because file name: {} is invalid' % image_name)
+            print('ignore the file, because file name: {%s} is invalid' % image_name)
             continue
         label_splits = image_name.split('_')
         if len(label_splits) > 2:
@@ -40,7 +41,37 @@ def generate_coor():
             continue
         label = label_splits[0]
         other = label_splits[1]
+        coors = other.split('-')
+        print('label: {0} - other: {1} - coors: {2}'.format(label, other, coors))
+        if len(coors) != 8:
+            print('ignore next coor action, because the coor: {0} is invalid'.format(coors))
+            continue
+        x1 = coors[0]
+        y1 = coors[1]
+        x2 = coors[2]
+        y2 = coors[3]
+        x3 = coors[4]
+        y3 = coors[5]
+        x4 = coors[6]
+        y4 = coors[7].split('.')
+        if len(y4) != 2:
+            print('ignore next y4 action, because y4: {0} is invalid'.format(y4))
+            continue
+        y4 = y4[0]
+        x_start = min([x1, x2, x3, x4])
+        y_start = min([y1, y2, y3, y4])
+        x_end = max([x1, x2, x3, x4])
+        y_end = max([y1, y2, y3, y4])
+        if x_start >= x_end or y_start >= y_end:
+            print(
+                'ignore next action,because coor value is invalid - x_start: {0} - y_start: {1} - x_end: {2} - y_end: {3}'
+                    .format(x_start, y_start, x_end, y_end))
+            continue
+        print(
+            'label: {0} - x_start: {1} - y_start: {2} - x_end: {3} - y_end: {4}'.format(label, x_start, y_start, x_end,
+                                                                                        y_end))
+        return label, (x_start, y_start, x_end, y_end)
 
 
 if __name__ == "__main__":
-    generate_coor()
+    generate_format_img()
